b81804993b245d5ab578157a46ad354e8c1692ee
Eliot <etroop719@gmail.com>
2018-08-04 10:44:50 -0700

    Created program, basically blank.

---
+++ snakeForms/snakeForms/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace snakeForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.snakeHead = new System.Windows.Forms.PictureBox();
+            this.gameTime = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.snakeHead)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // snakeHead
+            // 
+            this.snakeHead.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.snakeHead.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.snakeHead.Location = new System.Drawing.Point(0, 0);
+            this.snakeHead.Name = "snakeHead";
+            this.snakeHead.Size = new System.Drawing.Size(1276, 686);
+            this.snakeHead.TabIndex = 0;
+            this.snakeHead.TabStop = false;
+            this.snakeHead.Click += new System.EventHandler(this.snakeHead_Click);
+            // 
+            // gameTime
+            // 
+            this.gameTime.Enabled = true;
+            this.gameTime.Tick += new System.EventHandler(this.gameTime_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1276, 686);
+            this.Controls.Add(this.snakeHead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.snakeHead)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox snakeHead;
+        private System.Windows.Forms.Timer gameTime;
+    }
+}
+
---
+++ snakeForms/snakeForms/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snakeForms
+{
+    public partial class Form1 : Form
+    {
+        bool up = false;
+        bool down = false;
+        bool left = false;
+        bool right = false;
+        Graphics gfx;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        private void snakeHead_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        { 
+            if(e.KeyCode == Keys.Up)
+            {
+                up = true;
+
+            }
+            else
+            {
+                up = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                down = true;
+
+            }
+            else
+            {
+                down = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                left = true;
+
+            }
+            else
+            {
+                left = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                right = true;
+
+            }
+            else
+            {
+                right = false;
+            }
+        }
+
+        private void gameTime_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
