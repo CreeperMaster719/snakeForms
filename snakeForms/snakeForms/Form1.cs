e6d85631bf16ecc59d5e2bb2dae17231c070a478
Eliot <etroop719@gmail.com>
2018-09-15 10:46:22 -0700

    New fullsnake class

--- snakeForms/snakeForms/Form1.cs
+++ snakeForms/snakeForms/Form1.cs
@@ -19,6 +19,7 @@ namespace snakeForms
         Graphics gfx;
         Bitmap canvas;
         Random gen;
+        FullSnake Snake;
         List<SnakePiece> snakePieces = new List<SnakePiece>();
 
         Food firstfood;
@@ -34,7 +35,7 @@ namespace snakeForms
             snakePieces.Add(new SnakePiece(Brushes.Black, x, y, direction, length));
             gfx = Graphics.FromImage(canvas);
             firstfood = new Food(gen.Next(20, 1240), gen.Next(20, 590), length - 30, Brushes.Red);
-
+            Snake = new FullSnake(Brushes.Black, x, y, direction, length);
         }
         private void gameTime_Tick(object sender, EventArgs e)
         {
@@ -54,30 +55,49 @@ namespace snakeForms
             {
                 snakePieces[i].direction = snakePieces[i - 1].direction;
             }
+            for (int i = 1; i < snakePieces.Count; i++)
+            {
+                if (snakePieces[0].hitbox.IntersectsWith(snakePieces[i].hitbox))
+                {
+                    snakePieces[i].color = Brushes.Red;
+                    gameTime.Enabled = false;
+                }
+                else
+                {
+                    snakePieces[i].color = Brushes.Black;
+                }
+
+
+            }
             if (snakePieces[0].hitbox.IntersectsWith(firstfood.FoodBox))
             {
-                SnakePiece newpiece = new SnakePiece(Brushes.Black, snakePieces[snakePieces.Count - 1].x, snakePieces[snakePieces.Count - 1].y, snakePieces[snakePieces.Count - 1].direction, length);
+                /* SnakePiece newpiece = new SnakePiece(Brushes.Black, snakePieces[snakePieces.Count - 1].x, snakePieces[snakePieces.Count - 1].y, snakePieces[snakePieces.Count - 1].direction, length);
 
-                if (snakePieces[snakePieces.Count - 1].direction == 0)
-                {
-                    newpiece.y += length;
-                }
-                if (snakePieces[snakePieces.Count - 1].direction == 1)
-                {
-                    newpiece.y -= length;
-                }
-                if (snakePieces[snakePieces.Count - 1].direction == 2)
-                {
-                    newpiece.x += length;
-                }
-                if (snakePieces[snakePieces.Count - 1].direction == 3)
-                {
-                    newpiece.x -= length;
-                }
+                 if (snakePieces[snakePieces.Count - 1].direction == 0)
+                 {
+                     newpiece.y += length;
+                 }
+                 if (snakePieces[snakePieces.Count - 1].direction == 1)
+                 {
+                     newpiece.y -= length;
+                 }
+                 if (snakePieces[snakePieces.Count - 1].direction == 2)
+                 {
+                     newpiece.x += length;
+                 }
+                 if (snakePieces[snakePieces.Count - 1].direction == 3)
+                 {
+                     newpiece.x -= length;
+                 }
 
-                snakePieces.Add(newpiece);
+
+                 snakePieces.Add(newpiece);
+                 */
+                Snake.NewPiece();
                 firstfood.Move(gen);
             }
+
+
             for (int i = 0; i < snakePieces.Count; i++)
             {
                 snakePieces[i].Draw(gfx);
@@ -89,27 +109,55 @@ namespace snakeForms
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Up)
+            if (snakePieces.Count == 1)
             {
-                snakePieces[0].direction = 0;
+                if (e.KeyCode == Keys.Up)
+                {
+                    snakePieces[0].direction = 0;
+                }
+
+                if (e.KeyCode == Keys.Down)
+                {
+                    snakePieces[0].direction = 1;
+                }
+
+                if (e.KeyCode == Keys.Left)
+                {
+                    snakePieces[0].direction = 2;
+
+                }
+
+                if (e.KeyCode == Keys.Right)
+                {
+                    snakePieces[0].direction = 3;
+
+                }
+            }
+            else
+            {
+                if (e.KeyCode == Keys.Up && snakePieces[0].direction != 1)
+                {
+                    snakePieces[0].direction = 0;
+                }
+
+                if (e.KeyCode == Keys.Down && snakePieces[0].direction != 0)
+                {
+                    snakePieces[0].direction = 1;
+                }
+
+                if (e.KeyCode == Keys.Left && snakePieces[0].direction != 3)
+                {
+                    snakePieces[0].direction = 2;
+
+                }
+
+                if (e.KeyCode == Keys.Right && snakePieces[0].direction != 2)
+                {
+                    snakePieces[0].direction = 3;
+
+                }
             }
 
-            if (e.KeyCode == Keys.Down)
-            {
-                snakePieces[0].direction = 1;
-            }
-
-            if (e.KeyCode == Keys.Left)
-            {
-                snakePieces[0].direction = 2;
-
-            }
-
-            if (e.KeyCode == Keys.Right)
-            {
-                snakePieces[0].direction = 3;
-
-            }
 
         }
 
---
+++ snakeForms/snakeForms/FullSnake.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snakeForms
+{
+    class FullSnake
+    {
+       
+        public List<SnakePiece> Body = new List<SnakePiece>();
+        public SnakePiece Head
+        {
+            get
+            {
+                return Body[0];
+            }
+        }
+        public FullSnake(Brush color, int x, int y, int direction, int length)
+        {
+            SnakePiece snakePiece = new SnakePiece(color, x, y, direction, length);
+            Body.Add(snakePiece);
+          
+        }
+        //Update - updates the direction of all the pieces in the body
+
+        public void NewPiece()
+        {
+       
+                SnakePiece newpiece = new SnakePiece(Brushes.Black, Body[Body.Count - 1].x, Body[Body.Count - 1].y, Body[Body.Count - 1].direction, Head.length);
+
+                if (Body[Body.Count - 1].direction == 0)
+                {
+                    newpiece.y += Head.length;
+                }
+                if (Body[Body.Count - 1].direction == 1)
+                {
+                    newpiece.y -= Head.length;
+                }
+                if (Body[Body.Count - 1].direction == 2)
+                {
+                    newpiece.x += Head.length;
+                }
+                if (Body[Body.Count - 1].direction == 3)
+                {
+                    newpiece.x -= Head.length;
+                }
+
+
+                Body.Add(newpiece);
+
+                
+            }
+        }
+
+    //Draw - draws all the snakepieces
+
+
+    }
+
